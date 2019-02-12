d0370dbfedb64954800dca999646c76173aff5e2
Tomasz Bąk <tomaszbakk@outlook.com>
2019-02-12 21:50:34 +0100

    First test for nullable.

---
+++ CompareThis.UnitTests/CompareFactory_TestNullable.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Reflection;
+using CompareThis.Utilities.ExampleClass;
+
+namespace CompareThis.UnitTests
+{
+    /// <summary>
+    /// Summary description for CompareFactory_TestNullable
+    /// </summary>
+    [TestClass]
+    public class CompareFactory_TestNullable
+    {
+        private const string PropertyName = "DateTimeProperty";
+
+        [TestMethod]
+        public void TestCustomMethod_ReturnTrue()
+        {
+            var testClass = new BasicClass() { DateTimeProperty = new DateTime(2000, 10, 10) };
+            var prop = testClass.GetType().GetProperty(PropertyName);
+
+            Assert.IsTrue(Test(prop, testClass));
+        }
+
+        [TestMethod]
+        public void TestCustomMethod_ReturnFalse()
+        {
+            var testClass = new BasicClass() { };
+            var prop = testClass.GetType().GetProperty(PropertyName);
+
+            Assert.IsFalse(Test(prop, testClass));
+        }
+
+        // The idea is to "translate" this to expression.
+        public bool Test(PropertyInfo prop, object o)
+        {
+            if (!prop.PropertyType.IsGenericType || prop.PropertyType.GetGenericTypeDefinition() != typeof(Nullable<>))
+                return false;
+
+
+            var val = prop.GetValue(o);
+
+            return val != null;
+            // Expression
+        }
+
+
+
+    }
+}
